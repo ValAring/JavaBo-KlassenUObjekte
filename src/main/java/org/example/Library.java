203e0a32e01332bbd6ba6fc39724a5bdae76f16e
valeriearing <the.v.aring@gmail.com>
2023-08-29 17:06:13 +0200

    Changes Ü5

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -5,17 +5,20 @@ import java.util.Arrays;
 public class Library {
     private Book[] myBooks;
 
+
     public Library(){
-        myBooks = new Book[3];
-        myBooks[0] = new Book("Erste Hilfe Typographie", "Friedrich Forssman", "3-865321-474-3");
-        myBooks[1] = new Book("Color and Light", "James Gurney", "973-235432-9771-2");
-        myBooks[2] = new Book("Mobile Business", "Erich Staudt", "3-363117-12-X");
+        myBooks = new Book[10];
+        //myBooks[0] = new Book("Erste Hilfe Typographie", "Friedrich Forssman", "3-865321-474-3");
+        //myBooks[1] = new Book("Color and Light", "James Gurney", "973-235432-9771-2");
+        //myBooks[2] = new Book("Mobile Business", "Erich Staudt", "3-363117-12-X");
+    }
+
+    public void addBooksToLib(Book newBook){
+        myBooks[0] = newBook;
     }
 
     @Override
     public String toString() {
-        return "Library{" +
-                "books=" + Arrays.toString(myBooks) +
-                '}';
+        return "Books in my library: " + Arrays.toString(myBooks);
     }
 }
